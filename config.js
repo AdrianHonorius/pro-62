2e0ad14d8c2f9918dcb3d843b36cd8624e5f4a8b
AdrianHonorius <76515496+AdrianHonorius@users.noreply.github.com>
2022-01-23 12:44:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+ import * as firebase from "firebase";
+
+//initialize your database
+var firebaseConfig = {
+    const firebaseConfig = {
+  apiKey: "AIzaSyBpTwBGBslUXz_2DWZETW1vlCjcYYxkAx8",
+  authDomain: "school-attendance-c9614.firebaseapp.com",
+  databaseURL: "https://school-attendance-c9614-default-rtdb.firebaseio.com",
+  projectId: "school-attendance-c9614",
+  storageBucket: "school-attendance-c9614.appspot.com",
+  messagingSenderId: "437187868475",
+  appId: "1:437187868475:web:907c6b647c9ba2222a691e"
+};
+
+  export default firebase.database()
+ 
+
+  
